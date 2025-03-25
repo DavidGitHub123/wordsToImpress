f9de7c0f2f15822d051a19f5ffc251932c24617f
David Olsen <davidpaulolsen@gmail.com>
2025-03-25 16:16:17 -0600

    splashscreen

--- app/index.js
+++ app/index.js
@@ -3,6 +3,65 @@ import { NavigationContainer } from "@react-navigation/native";
 import NavigationLinks from "../Pages/NavigationLinks";
 import init from "../components/init";
 import { StyleSheet } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import logo from "../asset/logo_sm.png";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+      <Entypo name="rocket" size={30} />
+      <Image style={style.imageHeader} source={logo} />
+    </View>
+  );
+}
 
 function App() {
   useEffect(() => {
@@ -17,7 +76,7 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
 
 const style = StyleSheet.create({
   flex: { flex: 1 },
